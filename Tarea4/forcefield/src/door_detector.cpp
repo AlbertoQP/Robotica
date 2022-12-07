2e1419614b7f5d25a8864aaf7b990d3d88aaa5f5
daviid003 <dbarrosop@alumnos.unex.es>
2022-12-07 19:42:09 +0100

    door detection working

---
+++ Tarea4/forcefield/src/door_detector.cpp
@@ -0,0 +1,5 @@
+//
+// Created by alumno on 7/12/22.
+//
+
+#include "door_detector.h"
---
+++ Tarea4/forcefield/src/door_detector.h
@@ -0,0 +1,14 @@
+//
+// Created by alumno on 7/12/22.
+//
+
+#ifndef FORCEFIELD_DOOR_DETECTOR_H
+#define FORCEFIELD_DOOR_DETECTOR_H
+
+
+class door_detector {
+
+};
+
+
+#endif //FORCEFIELD_DOOR_DETECTOR_H
